415bf25a82bb7ac6f4f3b64cc8594927f2b04dd9
DESKTOP-FAHVR3K\conor <conor_a_murphy@outlook.com>
2016-11-08 10:36:38 +0000

    Location Tracker #1 Source Code
    Commit for tutorial #1 location tracker source code. Logic pulls through
    the latitude and logitude of the android emulator via the geolocator
    lib.

---
+++ LocationSampleApp/LocationSampleApp/LocationSampleApp.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("LocationSampleApp.Droid.MainApplication, LocationSampleApp.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md566f5c2214e1b975e96392a33048523a5.MainApplication.class, md566f5c2214e1b975e96392a33048523a5.MainApplication.__md_methods);
+		
+	}
+}
---
+++ LocationSampleApp/LocationSampleApp/LocationSampleApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+
+namespace LocationSampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnGetLocation.Clicked += BtnGetLocation_Clicked;
+        }
+
+        private async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            await RetreiveLocation();
+        }
+
+        private async Task RetreiveLocation() {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+
+            var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+            txtLat.Text = "Latitude: " + position.Latitude.ToString();
+            txtLong.Text = "Longitude: " + position.Longitude.ToString();
+        }
+    }
+}
